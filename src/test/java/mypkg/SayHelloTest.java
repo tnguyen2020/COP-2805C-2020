5f743213bad55ff51b58e7cca5a6c83f444e9827
Wayne Pollock <pollock@acm.org>
2020-01-28 00:02:03 -0500

    Initial version

---
+++ src/test/java/mypkg/SayHelloTest.java
@@ -0,0 +1,21 @@
+package mypkg;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+/**
+ * Empty test Suite for SayHello program.
+ *
+ * @author Wayne Pollock, Tampa Florida USA
+ */
+public class SayHelloTest {
+
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void testSayHello()
+    {
+        assertTrue( true );
+    }
+}
